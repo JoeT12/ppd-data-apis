a9050f5ef70e412e769d8feed84d2842b4106292
JoeT12 <joseph.taylor19@hotmail.com>
2026-02-15 17:20:37 +0000

    Creating First 2 APIs For Writing/Uploading Yearly PPD Data to Database, Querying Average House Price Per UK Town/City, Enforcing Access Control on The APIs, Setting Up ORM With Schema Generated By ChatGPT at Beginning of Project

--- src/main/java/com/uol/comp3011/Coursework1/Controller/DataReadController.java
+++ src/main/java/com/uol/comp3011/Coursework1/Controller/DataReadController.java
@@ -1,16 +1,46 @@
 package com.uol.comp3011.Coursework1.Controller;
 
+import com.uol.comp3011.Coursework1.Structs.AvgPricePerTown;
+import com.uol.comp3011.Coursework1.Service.DataReadService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/read")
 public class DataReadController {
-    // All data read API's should go into this class.
+  // All data read API's should go into this class.
+  private final DataReadService dataReadService;
+  private static final Logger log = LoggerFactory.getLogger(DataReadController.class);
 
-//    @GetMapping("/")
-//    public ResponseEntity<>
+  public DataReadController(DataReadService dataReadService) {
+    this.dataReadService = dataReadService;
+  }
 
+  @PreAuthorize("hasRole('USER')")
+  @GetMapping("/stats/area/lowestPriceAvgPerTownCity")
+  public ResponseEntity<?> lowestPriceAvgTownCity(
+      @RequestParam(required = false) LocalDate from,
+      @RequestParam(required = false) LocalDate to,
+      @RequestParam(required = false) Integer limit) {
+    try {
+      List<AvgPricePerTown> avg = dataReadService.getLowestPriceAvgTownCity(from, to, limit);
+      return ResponseEntity.ok().body(avg);
+    } catch (SQLException e) {
+      // Query returned no results - client passed in invalid value.
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      // Unexpected server side error. Good habit to put this at top level to prevent crashes.
+      log.error(e.getMessage());
+      return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+  }
 }
---
+++ src/main/java/com/uol/comp3011/Coursework1/Structs/AvgPricePerTown.java
@@ -0,0 +1,3 @@
+package com.uol.comp3011.Coursework1.Structs;
+
+public record AvgPricePerTown(String townCity, Double price) {}
